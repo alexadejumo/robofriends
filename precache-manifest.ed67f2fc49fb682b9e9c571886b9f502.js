f27ea3d4641b83602ab58ecde43285bb5d628d7a
Alex Adejumo <Teebillz07@gmail.com>
2020-01-09 09:31:05 +0100

    Updates

---
+++ precache-manifest.ed67f2fc49fb682b9e9c571886b9f502.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc39551a436d717aa4cc6f52c1dce8f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b73c8d24a45c7e2626e",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cf3609972520fa6c7690",
+    "url": "/static/css/main.14a3b1ea.chunk.css"
+  },
+  {
+    "revision": "3b73c8d24a45c7e2626e",
+    "url": "/static/js/2.c13d78d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c13d78d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf3609972520fa6c7690",
+    "url": "/static/js/main.9feda48d.chunk.js"
+  },
+  {
+    "revision": "01cfe46d64e7031e80ea",
+    "url": "/static/js/runtime-main.0a3b50ab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
